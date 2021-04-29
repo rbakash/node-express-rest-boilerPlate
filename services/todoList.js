de087da48d289c8691fb3eee98e2d10cf0130a79
Akash RB <rbakashcs@gmail.com>
2021-04-29 18:43:58 -0500

    Simple endpoint which returns JSON

--- services/todoList.js
+++ services/todoList.js
@@ -4,8 +4,7 @@ const devDB = require("../helper/db-helper")
 
 exports.addItem = async (req, res) => {
   try {
-    console.log("addItem",path.join(__dirname,'../', 'public', 'todoList-db','db.json'));
-    res.send("addItem");
+    res.send({name:"addItem"});
 
 
     // Create the json if its not there and if its there append the new item to the to do list
