ade2e9809e6b89fc03639f93452b686faa1e0d29
Akash RB <rbakashcs@gmail.com>
2021-04-29 18:06:11 -0500

    Debuging the route

--- routes/todolist.js
+++ routes/todolist.js
@@ -4,6 +4,9 @@ var todoListHandler = require('../services/todoList');
 
 /* POST Todolist Item */
 router.get('/add',function (req, res) {
+    console.log(req);
+    console.log('\n');
+    console.log(req.url);
     todoListHandler.addItem(req, res);
 });
 
