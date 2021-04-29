7c71e09a80abccaf5f0e66287201c2f8fe2c1e1a
Akash RB <rbakashcs@gmail.com>
2021-04-29 18:13:49 -0500

    404 route

--- routes/todolist.js
+++ routes/todolist.js
@@ -9,5 +9,10 @@ router.get('/add',function (req, res) {
     console.log(req.url);
     todoListHandler.addItem(req, res);
 });
-
+router.get('*', function(req, res){
+    console.log(req);
+    console.log('\n');
+    console.log(req.url);
+    res.send('what???', 404);
+  });
 module.exports = router;
